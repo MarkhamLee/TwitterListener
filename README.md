86287b7433869ff030ed1f4e8c219893cb3cd461
MarkhamLee <markhamlee@outlook.com>
2022-02-12 18:18:45 -0800

    Gathering tweets in real team via the Twitter API and applying sentiment analysis, intention is to be a tool that can track real time sentiment on a product or service
    Initial adding of files

---
+++ README.md
@@ -0,0 +1,30 @@
+## Real Time Twitter Streamer 
+
+### Listener(s) for gathering tweets and performing natural language processing in real time. 
+
+I built the initial versions of this based on simple curiousity around performing natural language processing on tweets, and what started out as basic Twitter scraper that would grab a sampling of all tweets turned into something that could be used to track sentiment in real time, gather research data, etc. 
+
+The repo's contents are as follows:
+* **Filter_Twitter_Listener_SQLite(FINAL).py:** a python script that will monitor twitter for the given set of keywords, run sentiment analysis and then write those tweets as they come in to a SQLite db. I used SQL because it was convenient, but you could drop in anything you'd like to store the tweets. 
+
+* **Sample_TwitterListener(SQLite)_FINAL.py:** works similar to the above only it takes in a roughly 1% sampling of all tweets, rather than tweets that fit a particular keyword. The data flow for this one can get rather intense as it's not uncommon to to see anywhere from 12 - 20 tweets per second coming in. Also: don't be surprised if running this one for an extended period of time makes your computer run hot. 
+
+* **Note:** I believe that the filtered tweet stream is limited to 1% of all tweets as well. 
+
+* **secrets.py:** this is a file where you store your Twitter Developer API access credentials. The included one in this repo is empty, you just need to drop in the appropriate credentials after you create your developer's account. I'd advise to apply for an enhanced account, as there won't be any limits on streamed tweets. 
+
+* **TwitterIngestion(FINAL).ipynb:** Jupyter notebook used to manage everything. Both listener scripts are pulled into the notebook as external classes. When you run either of the cells corresponding to each script, you will be promoted for # of keywords, keywords and # of tweets you'd like to retrieve depending on how you're gathering tweets. I also included some sample code for querying the SQLite DB, you can retrieve data by writing standard SQL queries within the pd.read_sql_query function. 
+
+* **requirements.txt:** a file listing all the libraries used for this project. 
+
+***Future enhancements are likely to include*** 
+* A real time dashboard showing items like tweet volume and sentiment by keyword. My plan is to build dashboards with both Django and Streamlit, the former because that is what I'm more likely to encounter at work and the latter because it looks like a really elegant way to quickly stand up dashboards so it's a technology worth learning. 
+* Better handling of connection issues, namely: I'd like the solution to pause when it receives a 420 or other error code and just wait for 5-10 minutes and then try to reconnect. 
+
+
+#### Equipment used: 16" 2019 MacBook Pro, eight core Intel i9, 16GB 
+
+#### Key Links:
+* [Twitter Developer Site](https://developer.twitter.com/en)
+* I leaned heavily on the [Tweepy documentation](https://docs.tweepy.org/en/stable/) 
+* The [Tweepy source code Github](https://github.com/tweepy/tweepy) was an invaluable resource, as noted earlier customization requires overriding a lot of the methods, plus it was just easier to understand how everything worked from studying the source code. 
---
+++ Sample_TwitterListener_FINAL.py
@@ -0,0 +1,143 @@
+import tweepy
+import pandas as pd
+import requests
+from datetime import datetime
+from textblob import TextBlob
+from platform import python_version
+from better_profanity import profanity
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import dataset
+
+# note: the imports platform and requests are to support the initializer
+# this class overrides many of the methods in the tweepy library in order 
+# to get my desired behavior, it's probably a good idea to look at the tweepy
+# source code if you plan to make significant customizations 
+
+class Sample(tweepy.Stream):   
+ 
+    def __init__(self, consumerKey, consumerSecret, accessToken, accessTokenSecret, tweet_count):
+        self.process_count = 0
+        self.consumer_key = consumerKey
+        self.consumer_secret = consumerSecret
+        self.access_token = accessToken
+        self.access_token_secret = accessTokenSecret
+        self.limit = tweet_count
+        self.chunk_size = 512
+        self.daemon = False 
+        self.max_retries = 100 
+        self.verify = True 
+        self.proxies = {"https": None} if None else {}
+        self.running = False
+        self.session = None
+        self.thread = None 
+        self.print_count = 0
+        self.user_agent = (
+            f"Python/{python_version()} "
+            f"Requests/{requests.__version__} "
+            f"Tweepy/{tweepy.__version__}")
+        
+    
+    def on_status(self, status):   
+
+        # add logic to close the connection after a certain number of tweets are retrieved
+        # otherwise the connection will stay open indefinitely or until there is an issue 
+        # that cause it to close since it's not a guaranteed pipe 
+        if self.process_count == self.limit:
+            
+            # exit app
+            self.disconnect(1)
+        
+        # removing retweets to help avoid duplicates 
+        if status.retweeted or 'RT @' in status.text:
+            return
+
+        #to do: add feature to remove tweets with sensitive content, e.g. videos or photos 
+
+        # acquiring location, converting coordinates to string as it's easier to handle 
+        location = status.coordinates 
+        if location:
+            location = str(status.coordinates['coordinates'])
+
+        # ensure we grab exteded tweets when available 
+        if hasattr(status,"extended_tweet"):
+            tweet_text = status.extended_tweet['full_text']
+        else:
+            tweet_text = status.text
+
+        # Commented out, but the feature to reject tweets containing profanity
+        # is here if desired  
+        #if profanity.contains_profanity(tweet_text):
+        #    return     
+       
+        # calculate sentiment - based on raw tweets
+        # may potentially remove certain characters in subsequent analysis 
+        blob = TextBlob(tweet_text)
+        tweet_sentiment = blob.sentiment
+        polarity = tweet_sentiment.polarity
+        subjectivity = tweet_sentiment.subjectivity 
+
+        # sentiment intensity tallying positive vs. neutral vs. negative 
+        # in order to make this more efficient I generate the score for later use in determining 
+        # if a tweet was positive vs. neutral vs. negative 
+        score = SentimentIntensityAnalyzer().polarity_scores(tweet_text)
+
+        # for more accurate analytics but displaying tweets in public
+        # censors profanity in the original tweet, but only after sentiment has been calculated 
+        # comment out when using the earlier code to reject all tweets with profanity 
+        tweet_text = profanity.censor(tweet_text)
+
+        # if you don't have the DB or table created, it will be created
+        # for you automatically
+        # insert your own names for the db and the table 
+        db = dataset.connect("sqlite:///sampleTweets.db")
+
+        table = db['sample_tweet_table']
+        table.insert(dict(
+            text=tweet_text,
+            timeStamp = status.created_at,
+            verified_user=status.user.verified,
+            followers=status.user.followers_count,
+            retweetCount = status.retweet_count,
+            favoriteCount = status.favorite_count,
+            location = location,
+            sentiment = polarity,
+            subjectivity = subjectivity,
+            score = score,))
+
+        #print tweet just to observe things working 
+        #print(tweet_text)
+
+        # increment the tweet counter and the counter for printing every nth tweet 
+        self.process_count+=1
+        self.print_count+=1 
+
+        # printing every 10th 
+        if self.print_count == 10:
+            print(tweet_text) # printing every 10th tweet 
+            self.print_count = 0 #reseting counter 
+
+    # manage errors and hitting the limit on retrieving tweets 
+    # limits wouldn't occur as the endpoint this class connects to 
+    # doesn't have a limit. However, there are limits as to the number of attempts to
+    # reconnect in case of a connection error 
+    def on_error(self, status_code):
+        
+        if status_code == 420:
+            # print error message
+            print("reached max limit and/or connection timed out")
+            
+            # exit app
+            self.disconnect(self.tweet_df, 0)
+            
+            
+    # ensures a more elegant disconnect sequence 
+    # the flag is meant to show a custom message instead of the "Twitter Disconnected" message
+    # needed to override the "on_disconnect()" to make that happen. 
+    def disconnect(self, flag):
+
+        if flag == 1:
+            print('stream disconnecting, desired sample size received')
+        else:
+            print('connection issue present, stream disconnecting')
+
+        self.running = False
